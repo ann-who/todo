8dec613d97ec1f7ec4f3827878e04cc5d945890c
Ann Impalovskaya <covertover@gmail.com>
2022-07-23 19:28:29 +0300

    Add icons and some constants

---
+++ lib/resources/app_constants.dart
@@ -0,0 +1,6 @@
+class WidgetsSettings {
+  static const double smallestScreenPadding = 8.0;
+
+  static const double cardElevation = 4.0;
+  static const double cardRadius = 8.0;
+}
---
+++ lib/resources/app_icons.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class IconResources {
+  static const String add = 'assets/app_icons/add.png';
+  static const String arrowBack = 'assets/app_icons/arrow_back.png';
+  static const String check = 'assets/app_icons/check.png';
+  static const String checked = 'assets/app_icons/checked.png';
+  static const String close = 'assets/app_icons/close.png';
+  static const String delete = 'assets/app_icons/delete.png';
+  static const String infoOutline = 'assets/app_icons/info_outline.png';
+  static const String unchecked = 'assets/app_icons/unchecked.png';
+  static const String uncheckedUrgent = 'assets/app_icons/unchecked_urgent.png';
+  static const String visibility = 'assets/app_icons/visibility.png';
+  static const String visibilityOff = 'assets/app_icons/visibility_off.png';
+}
+
+class AppIcon extends StatelessWidget {
+  final String path;
+  final Color? color;
+  final double? width;
+  final double? height;
+
+  const AppIcon({
+    required this.path,
+    this.color,
+    this.width,
+    this.height,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      path,
+      color: color,
+      width: width,
+      height: height,
+    );
+  }
+}
